713652771f63e76c0680740f8a6df7ef77732f16
qbs007 <keigo.imamura@qdenbs.com>
2020-07-13 13:27:39 +0900

    Initial Commit

---
+++ HelloWorld2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HelloWorld2
+//
+//  Created by 0007 QBS on 2020/07/13.
+//  Copyright © 2020 0007QBS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
